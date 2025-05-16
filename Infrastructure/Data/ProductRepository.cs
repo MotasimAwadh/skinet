ae34d0cb6451a07a1468796efde69d28cd1db02f
MotasimAwadh <107761643+MotasimAwadh@users.noreply.github.com>
2025-05-17 00:34:53 +0300

    End of Section 3 (17-MAY-2025)

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,75 @@
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ProductRepository(StoreContext context) : IProductRepository
+{
+    public void AddProduct(Product product)
+    {
+        context.Products.Add(product);
+    }
+
+    public void DeleteProduct(Product product)
+    {
+        context.Products.Remove(product);
+    }
+
+    public async Task<IReadOnlyList<string>> GetTypesAsync()
+    {
+        return await context.Products.Select(u => u.Type)
+            .Distinct()
+            .ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<string>> GetBrandAsync()
+    {
+        return await context.Products.Select(u => u.Brand)
+            .Distinct()
+            .ToListAsync();
+    }
+
+    public async Task<Product?> GetProductByIdAsync(int id)
+    {
+        return await context.Products.FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort)
+    {
+        var query = context.Products.AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(brand))
+            query = query.Where(u => u.Brand == brand);
+
+        if (!string.IsNullOrWhiteSpace(type))
+            query = query.Where(u => u.Type == type);
+
+        if (!string.IsNullOrWhiteSpace(sort))
+        {
+            query = sort switch
+            {
+                "priceAsc" => query.OrderBy(u => u.Price),
+                "priceDesc" => query.OrderByDescending(u => u.Price),
+                _ => query.OrderBy(u => u.Name)
+            };
+        }
+
+        return await query.ToListAsync();
+    }
+
+    public bool ProductExists(int id)
+    {
+        return context.Products.Any(u => u.Id == id);
+    }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        context.Entry(product).State = EntityState.Modified;
+    }
+}
